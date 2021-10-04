1074c7f700c550c8382c323737db6f7c19c8ebae
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2021-10-04 10:59:44 +0200

    Initial commit.

---
+++ src/main/java/no/ntnu/iiir/mobapp/api/librarylocatorapi/LibraryLocatorApiApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.iiir.mobapp.api.librarylocatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLocatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLocatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/iiir/mobapp/api/librarylocatorapi/LibraryLocatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.iiir.mobapp.api.librarylocatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLocatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
