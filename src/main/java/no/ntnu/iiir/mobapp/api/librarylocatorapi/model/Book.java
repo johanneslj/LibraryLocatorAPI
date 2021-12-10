cd0ab31e807a55761ef0209a1fe06fe3d868747a
Simon Duggal <simondu@stud.ntnu.no>
2021-12-10 13:06:32 +0100

    Made changes to class, added parameters

--- src/main/java/no/ntnu/iiir/mobapp/api/librarylocatorapi/model/Book.java
+++ src/main/java/no/ntnu/iiir/mobapp/api/librarylocatorapi/model/Book.java
@@ -7,12 +7,22 @@ package no.ntnu.iiir.mobapp.api.librarylocatorapi.model;
 public class Book {
   private Long id;
   private String isbn;
+  private String title;
+  private String author;
+  private String summary;
+  private String image;
 
-
-  public Book(Long id, String isbn) {
+  public Book(Long id,String isbn) {
     this.id = id;
     this.isbn = isbn;
+  }
 
+  public Book(String isbn, String title, String author, String summary, String image) {
+    this.isbn = isbn;
+    this.title = title;
+    this.author = author;
+    this.summary = summary;
+    this.image = image;
   }
 
   public Long getId() {
@@ -31,6 +41,37 @@ public class Book {
     this.isbn = isbn;
   }
 
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getSummary() {
+    return summary;
+  }
+
+  public void setSummary(String summary) {
+    this.summary = summary;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
 
   @Override
   public String toString() {
